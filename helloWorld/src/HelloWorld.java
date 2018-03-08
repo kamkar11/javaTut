ca5dedb433e90ec7cca80fb10f789e68313496d3
XYZ <XYZ@LAPTOP-7CM246A1>
2018-03-09 00:19:56 +0100

    Hello world poprawki

--- helloWorld/src/HelloWorld.java
+++ helloWorld/src/HelloWorld.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +17,20 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+        switch(n)
+        {
+            case 1:
+            {
+                System.out.println("Yes");
+            }
+        }
+
     }
-    
+
 }
